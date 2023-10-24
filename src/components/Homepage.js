de762c51eaedefbddf4f0295a3485ed81a3b7cfc
olivier-webdev <olivier.webdev@gmail.com>
2023-10-24 15:38:32 +0200

    mise en place searchBar

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,16 +1,36 @@
 import styles from "./Homepage.module.scss";
 import Serie from "./Serie";
 import { series } from "../data";
+import { useState } from "react";
 
 export default function Homepage() {
+  const [filter, setFilter] = useState("");
+
+  const handleInput = (e) => {
+    const search = e.target.value;
+    setFilter(search.trim().toLowerCase());
+  };
   return (
     <div className=" flex-fill container">
       <h1 className="mb20">Découvrez nos dernières critiques</h1>
-      <div className={`card p20 ${styles.contentCard}`}>
+      <div className={`card p20 mb20 d-flex flex-column ${styles.contentCard}`}>
+        <div
+          className={`d-flex justify-content-center align-items-center my30 ${styles.searchBar}`}
+        >
+          <i className="fas fa-magnifying-glass mr10"></i>
+          <input
+            onInput={handleInput}
+            className="flex-fill"
+            type="text"
+            placeholder="Search..."
+          />
+        </div>
         <div className={`${styles.grid}`}>
-          {series.map((serie) => (
-            <Serie key={serie.id} title={serie.title} image={serie.image} />
-          ))}
+          {series
+            .filter((serie) => serie.title.toLowerCase().includes(filter))
+            .map((serie) => (
+              <Serie key={serie.id} title={serie.title} image={serie.image} />
+            ))}
         </div>
       </div>
     </div>
